992a9635210fe6407359b47f498415adcae044b2
guyrawsthorn <guy.rawsthorn@btopenworld.com>
2024-12-08 22:31:38 +0000

    diagonals day 04

--- 04/index.py
+++ 04/index.py
@@ -3,20 +3,23 @@ import re
 count = 0
 
 def main():
-    grid = read_file_to_dict("test-input.txt")
+    grid = read_file_to_dict("real-input.txt")
     for row, values in grid.items():
         print(row)
         print(values)
         for list_index, item in enumerate(values):
             if item == "X":
                 print(f"found x at {list_index}")
-                # check_horizontal(row, values, list_index, direction="right")
-                # check_horizontal(row, values, list_index, direction="left")
-                # check_vertical(grid, row, values, list_index, direction="down")
-                # check_vertical(grid, row, values, list_index, direction="up")
+                check_horizontal(row, values, list_index, direction="right")
+                check_horizontal(row, values, list_index, direction="left")
+                
+                check_vertical(grid, row, values, list_index, direction="down")
+                check_vertical(grid, row, values, list_index, direction="up")
 
                 check_diagonal(grid, row, values, list_index, direction="se")
-                # check_diagonal(grid, row, values, list_index, direction="sw")
+                check_diagonal(grid, row, values, list_index, direction="sw")
+                check_diagonal(grid, row, values, list_index, direction="ne")
+                check_diagonal(grid, row, values, list_index, direction="nw")
 
     global count
     print(f"final count {count}")
@@ -27,14 +30,32 @@ def check_diagonal(grid, row, values, list_index, direction):
     # If we're going south east don't bother if your too far right or too low down.
     # 8 > (10 -3)
     # 7 > (9-3) = True | 6 > (9-3) = False
-    if direction=="se" and (list_index > len(values) -len("XMAS") -1 or row > (max(grid.keys())) - 3): 
+    if direction=="se" and (list_index > len(values) -3 or row > max(grid.keys()) - 3):
         return False
     
-    counter = Counter()
+    # If we're going south west don't bother if your too far left or too low down.
+    # 4 < (4-1) = True
+    # 7 > (9-3) = True | 6 > (9-3) = False
+    if direction=="sw" and (list_index < 3 or row > max(grid.keys()) - 3):
+        return False
     
-    if grid[row + counter.adjust("se")][list_index + counter.value] == "M":
-        if grid[row + counter.adjust("se")][list_index + counter.value] == "A":
-            if grid[row + counter.adjust("se")][list_index + counter.value] == "S":
+    # If we're going north east don't bother if too far right or too high.
+    # 4 < (4-1) = True
+    # 7 > (9-3) = True | 6 > (9-3) = False
+    if direction=="ne" and (list_index > len(values) - 3 or row < len("XMAS") -1):
+        return False
+    
+    # If we're going north west don't bother if too far left or too high.
+    # 4 < (4-1) = True
+    # 7 > (9-3) = True | 6 > (9-3) = False
+    if direction=="nw" and (list_index < len("XMAS") -1 or row < len("XMAS") -1):
+        return False
+    
+    counter = Counter(direction=direction)
+
+    if grid[row + counter.adjust()[1]][list_index + counter.x_value] == "M":
+        if grid[row + counter.adjust()[1]][list_index + counter.x_value] == "A":
+            if grid[row + counter.adjust()[1]][list_index + counter.x_value] == "S":
                 print(f"found valid {direction} diagonal XMAS on row {row} starting at {list_index}")
                 global count
                 count += 1
@@ -42,17 +63,20 @@ def check_diagonal(grid, row, values, list_index, direction):
 def check_vertical(grid, row, values, list_index, direction):
     print(f"analyzing {direction} vertical {row}, {list_index}")
 
-    # if down but last key
-    if direction=="down" and row == max(grid.keys()):
-        return False
-    if direction=="up" and row == 0:
+    # if down but too low
+    if direction=="down" and row > (max(grid.keys()) - 3):
+        # print(f"down - {row} is too low")
         return False
     
-    counter = Counter()
+    if direction=="up" and row < len("XMAS") -1:
+        # print(f"up - {row} is too high")
+        return False
+    
+    counter = Counter(direction=direction)
 
-    if grid[row + counter.adjust(direction)][list_index] == "M":
-        if grid[row + counter.adjust(direction)][list_index] == "A":
-            if grid[row + counter.adjust(direction)][list_index] == "S":
+    if grid[row + counter.adjust()[1]][list_index] == "M":
+        if grid[row + counter.adjust()[1]][list_index] == "A":
+            if grid[row + counter.adjust()[1]][list_index] == "S":
                 print(f"found valid {direction} vertical XMAS on row {row} starting at {list_index}")
                 global count
                 count += 1
@@ -60,18 +84,18 @@ def check_vertical(grid, row, values, list_index, direction):
 def check_horizontal(row, values, list_index, direction):
     print(f"analyzing {direction} horizonal {row}, {list_index}")
     
-    counter = Counter()
+    counter = Counter(direction=direction)
 
-    # if right but end of row
-    if direction=="right" and list_index == len(values) -1:
+    # if checking right but too far right
+    if direction=="right" and list_index > len(values) -len("XMAS") -1:
         return False
-    # if left but end of row
-    if direction=="left" and list_index == 0:
+    # if checking left but too far left
+    if direction=="left" and list_index < len("XMAS") -1:
         return False
 
-    if values[list_index + counter.adjust(direction)] == "M":
-        if values[list_index + counter.adjust(direction)] == "A":
-            if values[list_index + counter.adjust(direction)] == "S":
+    if values[list_index + counter.adjust()[0]] == "M":
+        if values[list_index + counter.adjust()[0]] == "A":
+            if values[list_index + counter.adjust()[0]] == "S":
                 print(f"found valid {direction} horizontal XMAS on row {row} starting at {list_index}")
                 global count
                 count += 1
@@ -84,21 +108,33 @@ def read_file_to_dict(file_path: str):
     return grid
 
 class Counter:
-    def __init__(self):
-        self.value = 0
+    def __init__(self, direction):
+        self.x_value = 0
+        self.y_value = 0
+        self.direction = direction
 
-    def adjust(self, direction):
-        if direction == "left":
-            self.value -= 1
-        elif direction == "right":
-            self.value += 1
-        elif direction == "down":
-            self.value += 1
-        elif direction == "up":
-            self.value -= 1
-        elif direction == "se":
-            self.value += 1
-        return self.value
+    def adjust(self):
+        if self.direction == "left":
+            self.x_value -= 1
+        elif self.direction == "right":
+            self.x_value += 1
+        elif self.direction == "down":
+            self.y_value += 1
+        elif self.direction == "up":
+            self.y_value -= 1
+        elif self.direction == "se":
+            self.x_value += 1
+            self.y_value += 1
+        elif self.direction == "sw":
+            self.x_value -= 1
+            self.y_value += 1
+        elif self.direction == "ne":
+            self.x_value += 1
+            self.y_value -= 1
+        elif self.direction == "nw":
+            self.x_value -= 1
+            self.y_value -= 1
+        return (self.x_value, self.y_value)
 
 
 if __name__ == "__main__":
